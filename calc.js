6585c8a554439be12a055495f4ac303e51474aef
vladincar <61913040+vladincar@users.noreply.github.com>
2023-03-29 16:44:44 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,1 +1,235 @@
-let select=document.querySelectorAll("select");select.forEach(a=>{a.addEventListener("change",()=>mainFunc())});let input=document.querySelectorAll("input");input.forEach(a=>{a.addEventListener("change",()=>mainFunc())});let innerRaskladka=document.querySelector(".innerRaskladka"),width=document.querySelector("#width"),height=document.querySelector("#height"),size=document.querySelector("#size"),price=document.querySelector("#price"),delMin;function mainFunc(){let e=document.querySelector("#paper"),b=document.querySelector("#dencity");document.querySelector("#front");let f=document.querySelector("#back");document.querySelector("#front2");let g=document.querySelector("#back2"),c=document.querySelector("#format"),a=document.querySelector("#ploternaya"),i=document.querySelector("#circulation");e.value.startsWith("\u041E\u0444\u0441\u0435\u0442\u043D\u044B\u0439")?(a.disabled=!0,a.checked=!1,c.checked=!0):a.disabled=!1,e.value.startsWith("\u041E\u0444\u0441\u0435\u0442\u043D\u044B\u0439")?(f.disabled=!1,g.disabled=!1):(f.disabled=!0,f.value="0",g.disabled=!0,g.value="\u0411\u0435\u0437 \u043B\u0430\u043C\u0438\u043D\u0430\u0446\u0438\u0438"),e.value.startsWith("\u041E\u0444\u0441\u0435\u0442\u043D\u044B\u0439")?(b.disabled=!1,"0"===b.value&&(b.value="80")):(b.disabled=!0,b.value="0"),440==width.value&&310==height.value||410==width.value&&290==height.value?size.value="\u04103+":size.value="\u0421\u0432\u043E\u0439 \u0440\u0430\u0437\u043C\u0435\u0440",c.checked?(innerRaskladka.style.width="440px",innerRaskladka.style.height="310px",width.min="35",height.min="35",width.max="440",height.max="310"):a.checked?(innerRaskladka.style.width="410px",innerRaskladka.style.height="290px",width.min="10",height.min="10",width.max="410",height.max="290",enforceMinMax(width),enforceMinMax(height)):a.checked||c.checked||a.click(),parseInt(width.value)<width.min?width.value=width.min:parseInt(height.value)<height.min&&(height.value=height.min);let j=Math.trunc(innerRaskladka.style.width.slice(0,3)/parseInt(width.value)),k=Math.trunc(innerRaskladka.style.height.slice(0,3)/parseInt(height.value)),l=j*k;for(x=0,innerRaskladka.innerHTML="";x<l;x++){let h=document.createElement("div");h.style.width=delMin>parseInt(width.value)?delMin+"px":width.value+"px",h.style.height=delMin>parseInt(height.value)?delMin+"px":height.value+"px",innerRaskladka.appendChild(h)}for(;innerRaskladka.getBoundingClientRect().bottom<innerRaskladka.lastElementChild.getBoundingClientRect().bottom;)innerRaskladka.removeChild(innerRaskladka.lastElementChild);let d=0;select.forEach(a=>{d+=+a.options[a.selectedIndex].getAttribute("calc")}),c.checked?d+=+c.getAttribute("calc"):a.checked&&(d+=+a.getAttribute("calc")),d*=Math.ceil(i.value/innerRaskladka.childElementCount),price.innerHTML=d<100?`100 грн.`:`${d.toFixed(0)} грн.`}function enforceMinMax(a){""!=a.value&&(parseInt(a.value)<parseInt(a.min)&&(delMin=a.min),parseInt(a.value)>parseInt(a.max)&&(a.value=a.max))}mainFunc()
+setTimeout(() => {
+    
+
+let arrForm = []
+
+
+
+//rerun script on change
+let select = document.querySelectorAll('select');
+select.forEach(x => {
+    x.addEventListener('change', () => mainFunc())
+})
+let input = document.querySelectorAll('input');
+input.forEach(x => {
+    x.addEventListener('change', () => mainFunc())
+})
+
+//
+
+let innerRaskladka = document.querySelector('.innerRaskladka');
+
+let width = document.querySelector('#width');
+let height = document.querySelector('#height');
+
+let size = document.querySelector('#size');
+let price = document.querySelector('#price');
+let delMin;
+
+
+//collect for send email
+
+
+
+
+function mainFunc() {
+
+    let paper = document.querySelector('#paper');
+    let dencity = document.querySelector('#dencity');
+
+    let colorFace = document.querySelector('#front');
+    let colorBack = document.querySelector('#back');
+
+    let laminaciyaFace = document.querySelector('#front2');
+    let laminaciyaBack = document.querySelector('#back2');
+
+    let format = document.querySelector('#format');
+    let ploternaya = document.querySelector('#ploternaya');
+
+    let circulation = document.querySelector('#circulation');
+
+    // Порезка если бумага то отключить плоттерную, самоклейка то выбрать плоттерную
+    if (paper.value.startsWith('Офсетный')) {
+        ploternaya.disabled = true;
+        ploternaya.checked = false;
+        format.checked = true;
+    } else {
+        ploternaya.disabled = false;
+    }
+
+
+    //При выборе самоклеек доступно только лицо
+    if (!paper.value.startsWith('Офсетный')) {
+        colorBack.disabled = true;
+        colorBack.value = '0';
+
+        laminaciyaBack.disabled = true;
+        laminaciyaBack.value = 'Без ламинации';
+    } else {
+        colorBack.disabled = false;
+        laminaciyaBack.disabled = false;
+    }
+
+    //При выборе самоклеек не доступна плотность
+    if (!paper.value.startsWith('Офсетный')) {
+        dencity.disabled = true;
+        dencity.value = '0';
+
+    } else {
+        dencity.disabled = false;
+        if (dencity.value === '0') dencity.value = '80';
+    }
+
+
+    //при изменении ширини или высоты А3+ меняетося на свой размер
+    if ((width.value == 440 && height.value == 310) || (width.value == 410 && height.value == 290)) {
+        size.value = "А3+";
+    } else {
+        size.value = "Свой размер";
+    }
+
+
+
+
+    ///////////////////////////Раскладка
+    //////////////////////////
+
+
+    //size raskladki
+    if (format.checked) {
+        innerRaskladka.style.width = '440px';
+        innerRaskladka.style.height = '310px';
+
+        //минимальный размер 
+
+        width.min = "35"
+        height.min = "35"
+
+
+        //максимальный размер 
+        width.max = "440"
+        height.max = "310"
+
+    } else if (ploternaya.checked) {
+        innerRaskladka.style.width = '410px';
+        innerRaskladka.style.height = '290px';
+
+        //минимальный размер
+
+        width.min = "10"
+        height.min = "10"
+
+        //максимальный размер 
+        width.max = "410"
+        height.max = "290"
+
+        enforceMinMax(width);
+        enforceMinMax(height);
+
+    } else if (!ploternaya.checked && !format.checked) {
+        ploternaya.click();
+    }
+
+    if ((parseInt(width.value)) < width.min) width.value = width.min
+    else if ((parseInt(height.value)) < height.min) height.value = height.min
+
+    //на лист сколько
+    let widthAmount = Math.trunc(innerRaskladka.style.width.slice(0, 3) / (parseInt(width.value)));
+    let heightAmount = Math.trunc(innerRaskladka.style.height.slice(0, 3) / (parseInt(height.value)));
+    let cardsAmount = widthAmount * heightAmount;
+    
+    //Reverse width and height and see where fit more
+    let checkIfReversed;
+    let widthAmountReversed = Math.trunc(innerRaskladka.style.height.slice(0, 3) / (parseInt(width.value)));
+    let heightAmountReversed = Math.trunc(innerRaskladka.style.width.slice(0, 3) / (parseInt(height.value)));
+    if((widthAmount*heightAmount)<(widthAmountReversed*heightAmountReversed)){
+        cardsAmount = widthAmountReversed*heightAmountReversed;
+        checkIfReversed=true;
+    }else checkIfReversed=false;
+
+
+    innerRaskladka.innerHTML = '';
+    for (x = 0; x < cardsAmount; x++) {
+        let div = document.createElement("div");
+        //если меньше минимального то не ставит на разметку ничего
+        if(!checkIfReversed){
+            div.style.width = delMin > parseInt(width.value) ? delMin + 'px' : width.value + 'px';
+            div.style.height = delMin > parseInt(height.value) ? delMin + 'px' : height.value + 'px';
+        }else{
+            div.style.height = delMin > parseInt(width.value) ? delMin + 'px' : width.value + 'px';
+            div.style.width = delMin > parseInt(height.value) ? delMin + 'px' : height.value + 'px';
+        }
+
+        innerRaskladka.appendChild(div);
+    }
+
+    //если дети выходят за пределы листа то удаляем их
+    while (innerRaskladka.getBoundingClientRect().bottom < innerRaskladka.lastElementChild.getBoundingClientRect().bottom) {
+        innerRaskladka.removeChild(innerRaskladka.lastElementChild);
+    }
+
+    /////////////СЧИТАТЬ СТОИМОСТЬ
+
+    let money = 0;
+    select.forEach(x => {
+        money += +x.options[x.selectedIndex].getAttribute("calc");
+    })
+
+    if (format.checked) money += +format.getAttribute("calc");
+    else if (ploternaya.checked) money += +ploternaya.getAttribute("calc");
+
+
+    money *= Math.ceil(circulation.value / innerRaskladka.childElementCount);
+
+    price.innerHTML = money < 100 ? `100 грн.` : `${money.toFixed(0)} грн.`
+
+
+
+
+    //info for email send
+    select.forEach((x, indx) => {
+        arrForm[indx + 1] = (x.value)
+    })
+    input.forEach((x, indx) => {
+        if (indx < 2) arrForm[indx] = (x.value)
+    })
+    if (format.checked) arrForm[8] = 'На формат'
+    else if (ploternaya.checked) arrForm[8] = 'Плоттерная'
+    arrForm[9] = circulation.value
+}; mainFunc();
+
+//если размер больше/меньше чем можно то ставит на максимально возможный
+function enforceMinMax(el) {
+    if (el.value != "") {
+        if (parseInt(el.value) < parseInt(el.min)) {
+            delMin = el.min;
+        }
+        if (parseInt(el.value) > parseInt(el.max)) {
+            el.value = el.max;
+        }
+    }
+}
+
+//add info to email
+let formText = `<div class="forminator-field forminator-merge-tags">
+<ul style="list-style-type:none">
+<li><span style="font-weight:600">Размеры - </span>${arrForm[0]}x${arrForm[1]} мм</li>
+<li><span style="font-weight:600">Бумага - </span>${arrForm[2]},${arrForm[3]}</li>
+<li><span style="font-weight:600">Цветность - </span>${arrForm[4]}+${arrForm[5]}</li>
+<li><span style="font-weight:600">Ламинация - </span>Лицо: ${arrForm[6]}, Оборот: ${arrForm[7]}</li>
+<li><span style="font-weight:600">Порезка - </span>${arrForm[8]}</li>
+<li><span style="font-weight:600">Тираж - </span>${arrForm[9]}</li>
+<li><span style="font-weight:600">Цена - </span>${price.innerHTML}</li>
+</ul>
+</div>`
+
+let formInfo = document.querySelector('.formInfo');
+formInfo.innerHTML = formText;
+
+//form appear onclick
+const form = document.querySelector('.elementor-section.elementor-top-section.elementor-element.elementor-element-b5a8f4b.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default');
+
+function formBtn(){
+    form.classList.toggle('elementorFormCustom')
+}
+}, 100);
